3075b82d6bdee34ceeaac78f4fec08868b7dcfd2
samsheybk2 <samireduardovasquezzambrano@gmail.com>
2026-06-18 15:08:10 -0400

    rename Dashboard -> Cierre de evaluaciones in sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -70,7 +70,7 @@ export function Sidebar() {
             }
           >
             <IconDashboard />
-            Dashboard
+            Cierre de evaluaciones
           </NavLink>
 
           {DIRECCIONES.map((dir) => {
